be03b96c4e027cbe6e2b43a92c4846d91816132c
GothicBat5 <nrejera.ccsjdm@gmail.com>
2026-07-26 14:47:01 +0800

    Create CAnimateSpecificTexture.cpp

---
+++ Valve/source/CAnimateSpecificTexture.cpp
@@ -0,0 +1,45 @@
+#include "cbase.h"
+#include "materialsystem/imaterialproxy.h"
+#include "materialsystem/imaterialvar.h"
+#include "materialsystem/imaterial.h"
+#include "materialsystem/itexture.h"
+#include "baseanimatedtextureproxy.h"
+#include "utlstring.h"
+#include <KeyValues.h>
+#include "tier0/memdbgon.h"
+
+class CAnimateSpecificTexture : public CBaseAnimatedTextureProxy
+{
+	private:
+
+	CUtlString m_OnlyAnimateOnTexture;
+
+	public:
+
+	virtual float GetAnimationStartTime( void* pBaseEntity ) { return 0; }
+	virtual bool Init( IMaterial *pMaterial, KeyValues *pKeyValues );
+	virtual void OnBind( void *pC_BaseEntity );
+	virtual void Release( void ) { delete this; }
+};
+
+bool CAnimateSpecificTexture::Init( IMaterial *pMaterial, KeyValues *pKeyValues )
+{
+	char const* pszAnimateOnTexture = pKeyValues->GetString( "onlyAnimateOnTexture" );
+  
+	if( !pszAnimateOnTexture ) return false;
+
+	m_OnlyAnimateOnTexture.Set( pszAnimateOnTexture );
+
+	return CBaseAnimatedTextureProxy::Init( pMaterial, pKeyValues );
+}
+
+void CAnimateSpecificTexture::OnBind( void *pC_BaseEntity )
+{
+	if( FStrEq( m_AnimatedTextureVar->GetTextureValue()->GetName(), m_OnlyAnimateOnTexture ) )
+	{
+		CBaseAnimatedTextureProxy::OnBind( pC_BaseEntity );
+	}
+	//else do nothing
+}
+
+EXPOSE_INTERFACE( CAnimateSpecificTexture, IMaterialProxy, "AnimateSpecificTexture" IMATERIAL_PROXY_INTERFACE_VERSION );
